472bb811a8913124035d43846c54fd63f9c9b9d4
alpden550 <alpden550@gmail.com>
2020-03-19 16:24:18 +0300

    Added departure's page.

--- tours/views.py
+++ tours/views.py
@@ -18,6 +18,25 @@ class MainView(TemplateView):
 class DepartureView(TemplateView):
     template_name = 'tours/departure.html'
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(DepartureView, self).get_context_data(*args, **kwargs)
+        departure = context.get('departure')
+
+        if departure not in tours_data.departures:
+            raise Http404
+
+        tours = {
+            tour_id: tour_data
+            for tour_id, tour_data in tours_data.tours.items()
+            if tour_data['departure'] == departure
+        }
+
+        context['tours'] = tours
+        context['departure'] = tours_data.departures.get(departure).split()[-1]
+        context['tours_prices'] = sorted([tour['price'] for tour in tours.values()])
+        context['tours_nights'] = sorted([tour['nights'] for tour in tours.values()])
+        return context
+
 
 class TourView(TemplateView):
     template_name = "tours/tour.html"
@@ -25,8 +44,10 @@ class TourView(TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super(TourView, self).get_context_data(*args, **kwargs)
         tour = tours_data.tours.get(context['tour_id'])
+
         if tour is None:
             raise Http404
+
         context['tour'] = tour
         context['departure'] = tours_data.departures.get(tour['departure']).split()[-1]
         return context
